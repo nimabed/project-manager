caf4466c5fc2929a50db7054c8a9a3c400724964
Nima <nima.abed.ch@gmail.com>
2026-01-01 14:30:49 +0330

    git initial

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="m-h-screen grid grid-cols-[250px_1fr]">
+      <section className="ring ring-gray-300">
+        sidebar
+      </section>
+
+
+      <main className="ring ring-indigo-300">
+        main
+      </main>
+    </div>
+  )
+}
+
+export default App
