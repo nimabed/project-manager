1f63b67d249eaf804ea3cdb535bad58e60480fa3
Nima <nima.abed.ch@gmail.com>
2026-01-03 10:46:16 +0330

    Sidebar component added!

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
+import Sidebar from './components/Sidebar';
+
 
 import './App.css'
 
 function App() {
   return (
-    <div className="m-h-screen grid grid-cols-[250px_1fr]">
-      <section className="ring ring-gray-300">
-        sidebar
+    <div className="min-h-screen grid grid-cols-[400px_1fr]">
+      <section className="pt-10">
+        <Sidebar />
       </section>
 
 
-      <main className="ring ring-indigo-300">
+      <main className="">
         main
       </main>
     </div>
